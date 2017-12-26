db6e6d3f17b18750adb710bf80d50e4057a4d171
vinhan16sep <vinhan16sep@gmail.com>
2017-12-26 18:04:21 +0700

    [in progress] update validate and error message

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -171,19 +171,9 @@ class CategoryController extends Controller
 
     private function validateInput($request) {
         $this->validate($request, [
-            'name' => 'required|max:60',
-//            'price' => 'required|max:60',
-//            'middlename' => 'required|max:60',
-//            'address' => 'required|max:120',
-//            'city_id' => 'required',
-//            'state_id' => 'required',
-//            'country_id' => 'required',
-//            'zip' => 'required|max:10',
-//            'age' => 'required',
-//            'birthdate' => 'required',
-//            'date_hired' => 'required',
-//            'department_id' => 'required',
-//            'division_id' => 'required'
+            'name' => 'required|max:255',
+            'slug' => 'required|unique:product_category|max:255',
+            'trademark_id' => 'required'
         ]);
     }
 
