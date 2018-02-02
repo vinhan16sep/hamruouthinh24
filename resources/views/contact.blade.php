bfde2a3bb06b447164c7125daf50a99925a507bc
minhtruongmato <truong.do@matocreative.vn>
2018-02-02 17:45:51 +0700

    [done] send mail contact in frontend

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.frontend-template')
 @section('content')
     <link href="{{ asset("public/frontend/css/contact.css")}}" rel="stylesheet" type="text/css" />
-    <section class="main_content" ng-controller="DetailBlogController">
+    <section class="main_content" ng-controller="ContactController">
         <div class="container">
             <div class="row">
                 <div class="contact-navside col-md-4 col-sm-4 col-xs-12">
@@ -23,29 +23,31 @@
                     <div class="content-title">
                         <h2>Liên hệ với chúng tôi</h2>
                     </div>
-                    <div class="contact-form">
-                        <label for="inputName">Họ tên (*)</label>
-                        <input type="type" class="form-control" id="inputName" placeholder="Họ tên">
-                        <br>
-                        <label for="inputEmail">Email (*)</label>
-                        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                        <br>
-                        <label for="inputPhone">Số điện thoại (*)</label>
-                        <input type="type" class="form-control" id="inputPhone" placeholder="Số điện thoại">
-                        <br>
-                        <label for="inputPhone">Lý do bạn đang cần liên hệ</label>
-                        <select class="form-control" id="inputReason">
-                            <option>Bạn đang cần liên hệ với chúng tôi vì...</option>
-                            <option>Tôi đang cần tư vấn làm đẹp</option>
-                            <option>Tôi đang cần tư vấn về sản phẩm</option>
-                            <option>Tôi muốn tìm hiểu về ABC</option>
-                            <option>Tôi muốn tìm hiểu về DEF</option>
-                        </select>
-                        <br>
-                        <textarea class="form-control" rows="4" placeholder="Tin nhắn..."></textarea>
-                        <br>
-                        <button type="submit" class="btn btn-default btn-fill">Gửi đi</button>
-                    </div>
+                    <form>
+                        <div class="contact-form">
+                            <label for="inputName">Họ tên (*)</label>
+                            <input type="type" class="form-control" id="inputName" placeholder="Họ tên" ng-model="contact.name">
+                            <br>
+                            <label for="inputEmail">Email (*)</label>
+                            <input type="email" class="form-control" id="inputEmail" placeholder="Email" ng-model="contact.email">
+                            <br>
+                            <label for="inputPhone">Số điện thoại (*)</label>
+                            <input type="type" class="form-control" id="inputPhone" placeholder="Số điện thoại" ng-model="contact.phone">
+                            <br>
+                            <label for="inputPhone">Lý do bạn đang cần liên hệ</label>
+                            <select class="form-control" id="inputReason" ng-model="contact.reason">
+                                <option>Bạn đang cần liên hệ với chúng tôi vì...</option>
+                                <option>Tôi đang cần tư vấn làm đẹp</option>
+                                <option>Tôi đang cần tư vấn về sản phẩm</option>
+                                <option>Tôi muốn tìm hiểu về ABC</option>
+                                <option>Tôi muốn tìm hiểu về DEF</option>
+                            </select>
+                            <br>
+                            <textarea class="form-control" rows="4" placeholder="Tin nhắn..." ng-model="contact.content"></textarea>
+                            <br>
+                            <input type="submit" ng-click="send(contact)" class="btn btn-default" value="Gửi đi" />
+                        </div>
+                    </form>
                 </div>
 
                 <div class="col-md-8 col-sm-8 col-xs-12">
@@ -55,7 +57,7 @@
         </div>
         <!-- InstanceEndEditable -->
     </section>
-    <script src="{{ asset ("public/frontend/app/controllers/detail-blog.js") }}"></script>
+    <script src="{{ asset ("public/frontend/app/controllers/contact.js") }}"></script>
     <script>
         $(window).scroll(function () {
             //if you hard code, then use console
